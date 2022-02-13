48dd59c86509a8e2fd8c4227e3032f1babc6fc8d
minster-hatter <t.n.hartley@gmail.com>
2022-02-13 13:35:02 +0000

    add data file to tracking

---
+++ data/make_db.py
@@ -0,0 +1,17 @@
+from sqlite3 import connect
+
+from pandas import read_csv
+
+cnxn = connect("stats_rethink.db")
+
+#howell = read_csv("Howell1.csv", delimiter=";")
+#print(howell)
+#howell.to_sql("howell1", cnxn, if_exists= "fail", index=False)
+
+
+# Not quite correct.
+#files = glob("*.csv")
+#for file in files:
+#    df = read_csv(file, delimiter=";")
+#    table_name = file.rsplit(".")[0]
+#    df.to_sql(table_name, cnxn, if_exists="replace", index=False)
