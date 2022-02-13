b1d0b668e1910d1c199f0ac551dbf43f736e638b
minster-hatter <t.n.hartley@gmail.com>
2022-02-13 13:21:24 +0000

    add height directory and import data

---
+++ height/height.py
@@ -0,0 +1,9 @@
+from pathlib import Path
+from sqlite3 import connect
+
+from pandas import read_sql
+
+# Add database location to the path and import.
+data_path = Path(__file__).parent / "../data/stats_rethink.db"
+cnxn = connect(data_path)
+howell_data = read_sql("SELECT * FROM howell1", cnxn)
