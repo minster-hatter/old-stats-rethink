ec6a3e05a5a4acf4867609c5812eb79aa9fb4716
minster-hatter <t.n.hartley@gmail.com>
2022-02-13 13:54:03 +0000

    height.py imports and model description

--- height/height.py
+++ height/height.py
@@ -2,11 +2,21 @@ from pathlib import Path
 from sqlite3 import connect
 
 from pandas import read_sql
+from pymc3 import Model, Normal, Uniform, sample_prior_predictive, sample, sample_posterior_predictive
+froma arviz import from_pymc3, summary, plot_ppc
 
 # Add database location to the path and import.
 data_path = Path(__file__).parent / "../data/stats_rethink.db"
 cnxn = connect(data_path)
 howell_data = read_sql("SELECT * FROM howell1", cnxn)
+adults_data = howell_data[howell_data["age"] >= 18]
 
 # Summarise and explore the data.
-print(howell_data.describe())
+print("Summary of the Howell data for adults:\n", adults_data.describe(), "\n")
+
+with Model() as m_4_1:
+    """h_i ~ Normal(mu, sigma)
+    mu ~ Normal(178, 20)
+    sigma ~ Uniform(0, 50)
+    """
+    None
