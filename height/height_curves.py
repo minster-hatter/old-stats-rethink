0497cc32ca3c2d6d22015a0baf8bc74bcfddc45e
minster-hatter <t.n.hartley@gmail.com>
2022-03-07 10:51:10 +0000

    create m_4_4 and its summary

--- height/height_curves.py
+++ height/height_curves.py
@@ -24,11 +24,37 @@ howell_data["s_weight"] = (
 ) / howell_data["weight"].std()
 howell_data["s_weight_squared"] = howell_data["s_weight"] ** 2
 
+# Linear model.
+with Model() as m_4_4:
+    """h_i ~ Normal(mu_i, sigma)
+    mu_i = alpha + beta * x_i
+    alpha ~ Normal(178, 20)
+    beta ~ Normal(0, 1)
+    sigma ~ Uniform(0, 50)
+    """
+    # Priors.
+    alpha = Normal("alpha", 178.0, 20.0)
+    beta = Normal("beta", 0.0, 1.0)
+    sigma = Uniform("sigma", 0.0, 50.0)
+    # Likelihood.
+    mu_i = alpha + beta * howell_data["s_weight"]
+    h_i = Normal("h_i", mu_i, sigma, observed=howell_data["height"])
+    # Sample and extract.
+    trace_m_4_4 = sample(SAMPLES, chains=CHAINS)
+    idata_m_4_4 = from_pymc3(trace_m_4_4, model=m_4_4)
+
+model_to_graphviz(m_4_4).render("m_4_4_dag", cleanup=True, format="png")
+
+summary(idata_m_4_4, hdi_prob=CI, stat_funcs=[median]).to_csv(
+    "m_4_4_summary.csv"
+)
+
+# Quadratic model.
 with Model() as m_4_5:
     """h_i ~ Normal(mu_i, sigma)
     mu_i = alpha + beta_1 * x_i + beta_2 * (x_i)^2
     alpha ~ Normal(178, 20)
-    beta_1 ~ Normal(0, 1)
+    beta_1 ~ Log-Normal(0, 1)
     beta_2 ~ Normal(0, 1)
     sigma ~ Uniform(0, 50)
     """
