14e9fa19d9c04fbc26c182ff792f7404e2c11ac6
minster-hatter <t.n.hartley@gmail.com>
2022-04-11 21:35:40 +0100

    re-make milk table in database and read into milk.py

--- milk/milk.py
+++ milk/milk.py
@@ -1,4 +1,8 @@
 from configparser import ConfigParser
+from pathlib import Path
+from sqlite3 import connect
+
+from pandas import read_sql
 
 # Constants to be used later.
 config = ConfigParser()
@@ -7,3 +11,9 @@ SAMPLES = config.getint("parameters", "SAMPLES")
 CHAINS = config.getint("parameters", "CHAINS")
 PREDICTIVE_SAMPLES = config.getint("parameters", "PREDICTIVE_SAMPLES")
 CI = config.getfloat("parameters", "CREDIBLE_INTERVAL")
+
+# Add database location to the path and import.
+data_path = Path(__file__).parent / "../data/stats_rethink.db"
+cnxn = connect(data_path)
+milk_data = read_sql("SELECT * FROM milk", cnxn)
+print(milk_data.describe())
