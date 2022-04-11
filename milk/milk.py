9cf343badaf618f8f4c061f4d5f593294960ba1c
minster-hatter <t.n.hartley@gmail.com>
2022-04-11 21:09:36 +0100

    create milk.py and directory

---
+++ milk/milk.py
@@ -0,0 +1,9 @@
+from configparser import ConfigParser
+
+# Constants to be used later.
+config = ConfigParser()
+config.read("../config.ini")
+SAMPLES = config.getint("parameters", "SAMPLES")
+CHAINS = config.getint("parameters", "CHAINS")
+PREDICTIVE_SAMPLES = config.getint("parameters", "PREDICTIVE_SAMPLES")
+CI = config.getfloat("parameters", "CREDIBLE_INTERVAL")
